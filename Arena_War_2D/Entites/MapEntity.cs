44bb6ee3a18dfedb2bd17f7854967851a95d85c1
savageRUN <109674251+savageRUN@users.noreply.github.com>
2022-09-27 18:18:43 +0300

    Initial commit

---
+++ Arena_War_2D/Entites/MapEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena_War_2D.Entites
+{
+    public class MapEntity
+    {
+        public PointF position;
+        public Size size;
+
+        public MapEntity(PointF pos, Size size)
+        {
+            this.position = pos;
+            this.size = size;
+        }
+    }
+}
